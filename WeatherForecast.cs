14cfe78da7b540144acf060309a6e5ea606cbe53
oliver-stone-dev <oliver.stone.dev@outlook.com>
2024-11-20 09:52:26 +0000

    Copy Init Files
    Copied initial build files from fresh VS project.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace film_friendly_airports_app
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
